bde7eeac5c679317d60739df7a0c8c9e5e2f717b
SAQ <SAQ@DESKTOP-091IM4F>
2024-12-17 17:52:21 +0530

    new one commit

---
+++ YTProject/src/main/java/com/example/YTProject/YtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.YTProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtProjectApplication.class, args);
+	}
+
+}
